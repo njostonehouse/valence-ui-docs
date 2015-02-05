3c0c162ed613b60667f458a381a77765c53f6ef2
Barbara Paduch <Barbara.Paduch@D2L.com>
2015-02-05 10:24:59 -0500

    Changed order of CSS files so that list icon style isn't overridden. Added JavaScript file for List States example.

---
+++ .static/vui-list-states-example.js
@@ -0,0 +1,37 @@
+( function() {
+
+	'use strict';
+
+	var $list = $( '#liststates' );
+
+	var updateListSelection = function() {
+
+		$list.find( 'li.vradio > label > input' ).each(
+			function ( index, inputNode ) {
+				
+				var $input = $( inputNode );
+
+				$input.closest( 'li.vradio' ).toggleClass(
+					'selected', $input.prop( 'checked' )
+				);
+
+			}
+		);
+
+	};
+
+	updateListSelection();
+
+	$list.change( function( args ) {
+
+		var $target = $( args.target );
+
+		 if ( $target.attr( 'type' ) === 'radio' ) {
+		 	
+			updateListSelection();
+		}
+
+	} );
+
+} )();
+
